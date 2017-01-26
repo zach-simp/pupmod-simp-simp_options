323abd288a883babbad464b585de1212c87e29a0
Dylan Cochran <heliocentric@gmail.com>
2017-01-25 21:46:44 -0500

    (SIMP-2336) Add simp_options::puppet::server_distribution (#11)
    SIMP-2336 #close

--- manifests/puppet.pp
+++ manifests/puppet.pp
@@ -9,12 +9,15 @@
 #
 # @param ca_port The port on which the Puppet Certificate Authority will listen
 #
+# @param server_distribution The server distribution being used, PC1 or PE.
+#
 # @author SIMP Team - https://simp-project.com
 #
 class simp_options::puppet (
   Simplib::Host $server,
   Simplib::Host $ca,
-  Simplib::Port $ca_port = 8141
+  Simplib::Port $ca_port = 8141,
+  Simplib::Serverdistribution $server_distribution = 'PC1'
 ){
   assert_private()
   validate_net_list($server)
