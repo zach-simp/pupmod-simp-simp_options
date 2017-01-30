77dc0a75704626eaefcb38598f38017281682177
Nick Miller <nick.miller@onyxpoint.com>
2017-01-30 11:25:35 -0500

    (SIMP-2548) Utilize fips_ciphers (#13)
    * (SIMP-2548) Utilize fips_ciphers
    
    Set the default value for openssl::cipher_suite to be the expanded value
    of the fips_ciphers fact.
    
    SIMP-2548 #close
    
    * - Updated changelog
    - Updated spec tests
    
    * Fails if fips is on and the fact is not found
    
    * Update CHANGELOG

--- manifests/openssl/params.pp
+++ manifests/openssl/params.pp
@@ -7,9 +7,16 @@ class simp_options::openssl::params (
 ) {
   assert_private()
   include '::simp_options'
+
   if $::simp_options::fips or $facts['fips_enabled'] {
-    $cipher_suite = ['FIPS', '!LOW']
-  } else {
+    if $facts['fips_ciphers'] {
+      $cipher_suite = $facts['fips_ciphers']
+    }
+    else {
+      fail('$facts[\'fips_ciphers\'] not found')
+    }
+  }
+  else {
     $cipher_suite = ['DEFAULT', '!MEDIUM']
   }
 }
--- spec/classes/openssl_spec.rb
+++ spec/classes/openssl_spec.rb
@@ -3,28 +3,35 @@ require 'spec_helper'
 describe 'simp_options' do
   context 'supported operating systems' do
     on_supported_os.each do |os, os_facts|
-
       context "on #{os}" do
 
-        context 'defaults for simp_options::openssl with FIPS enabled' do
-          let(:facts){{ :fips_enabled => true }}
-          it { is_expected.to compile.with_all_deps }
-          it { 
-            is_expected.to contain_class('simp_options::openssl').with(
-              :cipher_suite => ['FIPS', "!LOW"]
-            )
-          }
-        end
-
-        context 'defaults for simp_options::openssl without FIPS enabled' do
-          let(:facts){{ :fips_enabled => false }}
+        context 'defaults' do
           it { is_expected.to compile.with_all_deps }
           it {
             is_expected.to contain_class('simp_options::openssl').with(
               :cipher_suite => ['DEFAULT', '!MEDIUM']
             )
-         }
+          }
         end
+
+        context 'defaults for simp_options::openssl with FIPS enabled and the fips_ciphers fact unavailable' do
+          let(:facts){{ :fips_enabled => true }}
+          it { expect { is_expected.to compile.with_all_deps }.to raise_error(/not found/) }
+        end
+
+        context 'defaults for simp_options::openssl with FIPS enabled and the fips_ciphers fact available' do
+          let(:facts){{
+            :fips_enabled => true,
+            :fips_ciphers => ["ECDHE-RSA-AES256-GCM-SHA384","ECDHE-ECDSA-AES256-GCM-SHA384","other totally legit FIPS cipher"]
+          }}
+          it { is_expected.to compile.with_all_deps }
+          it {
+            is_expected.to contain_class('simp_options::openssl').with(
+              :cipher_suite => ["ECDHE-RSA-AES256-GCM-SHA384","ECDHE-ECDSA-AES256-GCM-SHA384","other totally legit FIPS cipher"]
+            )
+          }
+        end
+
       end
     end
   end
